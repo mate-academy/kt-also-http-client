f6b32f4fbf66dbf57e5e29bfe0f96ca1295cfff5
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-21 11:34:22 +0300

    feat [kotlin]: implement http request processing mock

--- src/main/kotlin/mate/academy/HttpRequestProcessor.kt
+++ src/main/kotlin/mate/academy/HttpRequestProcessor.kt
@@ -1,7 +1,22 @@
 package mate.academy
 
+private const val SUCCESS_STATUS = 200
+
 class HttpRequestProcessor(private val client: HttpClient) {
     fun processRequest(url: String): ResponseData {
-        // TODO: implement
+        val (statusCode, _, content) = client.sendRequest(url)
+            .also {
+                println("Response Status: ${it.statusCode} - ${it.statusText}")
+                println("Response Content: ${it.content}")
+            }
+            .also {
+                if (it.statusCode == SUCCESS_STATUS) {
+                    println("Processing content: ${it.content}")
+                }
+            }
+        return when (statusCode) {
+            SUCCESS_STATUS -> ResponseData("Success", content)
+            else -> ResponseData("Failure", "Request failed with status: $statusCode")
+        }
     }
 }
