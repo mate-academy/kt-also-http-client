6a1d3f98474510236791d61fc01d69287317f541
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-10-05 12:54:55 +0300

    Implemented the feature

--- src/main/kotlin/mate/academy/HttpRequestProcessor.kt
+++ src/main/kotlin/mate/academy/HttpRequestProcessor.kt
@@ -2,6 +2,22 @@ package mate.academy
 
 class HttpRequestProcessor(private val client: HttpClient) {
     fun processRequest(url: String): ResponseData {
-        // TODO: implement
+        val response = client.sendRequest(url).also {
+            println("Response Status: ${it.statusCode} - ${it.statusText}")
+            println("Response Content: ${it.content}")
+        }
+
+        if (response.statusCode == 200) {
+            response.also {
+                println("Processing content: ${response.content}")
+            }
+
+            return ResponseData("Success", response.content)
+        } else {
+            response.also {
+                return ResponseData("Failure",
+                    "Request failed with status: ${response.statusCode}")
+            }
+        }
     }
 }
