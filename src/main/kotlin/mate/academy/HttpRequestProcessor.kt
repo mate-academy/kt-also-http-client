46884a5f2565c7d675416fd9dbd52f94de49e014
ilko-ilya <samilyakilya@gmail.com>
2024-10-28 12:17:42 +0200

    First commit

--- src/main/kotlin/mate/academy/HttpRequestProcessor.kt
+++ src/main/kotlin/mate/academy/HttpRequestProcessor.kt
@@ -1,7 +1,20 @@
 package mate.academy
 
+const val HTTP_OK = 200
+
 class HttpRequestProcessor(private val client: HttpClient) {
     fun processRequest(url: String): ResponseData {
-        // TODO: implement
+        val response = client.sendRequest(url).also {
+           println("Response Status: ${it.statusCode} - ${it.statusText}")
+           println("Response Content: ${it.content}")
+        }
+        return when (response.statusCode) {
+            HTTP_OK -> {
+                response.also { println("Processing content: ${it.content}") }
+                ResponseData("Success", response.content)
+            }
+            else -> ResponseData("Failure", "Request failed with status: ${response.statusCode}")
+        }
     }
 }
+
