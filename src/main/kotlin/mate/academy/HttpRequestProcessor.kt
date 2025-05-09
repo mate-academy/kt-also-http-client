b472a8618e4527527d4639fce135765a0a039931
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2025-05-09 17:04:18 +0300

    solved task

--- src/main/kotlin/mate/academy/HttpRequestProcessor.kt
+++ src/main/kotlin/mate/academy/HttpRequestProcessor.kt
@@ -1,7 +1,22 @@
 package mate.academy
 
+private const val OK_STATUS_CODE = 200
+
 class HttpRequestProcessor(private val client: HttpClient) {
     fun processRequest(url: String): ResponseData {
-        // TODO: implement
+        return client.sendRequest(url)
+            .also {
+                println("Response Status: ${it.statusCode} - ${it.statusText}")
+                println("Response Content: ${it.content}")
+            }.let { response ->
+                if (response.statusCode == OK_STATUS_CODE) {
+                    response.also {
+                        println("Processing content: ${it.content}")
+                    }
+                    ResponseData("Success", response.content)
+                } else {
+                    ResponseData("Failure", "Request failed with status: ${response.statusCode}")
+                }
+            }
     }
 }
